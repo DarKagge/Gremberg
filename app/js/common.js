4242bb5f5daadfc4bda6e293292c866d7e2044b9
jeka <ElectroDeggi33759@yandex.ru>
2017-01-19 12:02:34 +0200

    1

--- app/js/common.js
+++ app/js/common.js
@@ -118,3 +118,12 @@ $(function() {
 		type:'inline',
 		midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
 	});
+
+
+
+$("a[href*='#']").mPageScroll2id();
+
+
+
+
+
